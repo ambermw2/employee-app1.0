8486f111ae87fd639fd3e142dea5d22748c0e4a8
20112953 <ambermullan18@gmail.com>
2025-09-17 13:46:21 +0100

    Completed Version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,86 @@
 package ie.setu
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import javax.naming.Name
+import javax.print.event.PrintJobEvent
+import kotlin.math.round
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+
+
+val firstName = "Joe"
+val surname = "Soap"
+val gender = 'm'
+val employeeID = 6143
+val grossSalary = 67543.21
+val payePercentage = 38.5
+val prsiPercentage = 5.2
+val annualBonus = 1450.50
+val cycleToWorkMonthlyDeduction = 54.33
+
+
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readln().toInt()
 }
+
+fun main(args: Array<String>){
+
+    var input : Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 ->println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+}
+
+fun getPayslip() = """                       
+    _______________________________________________________________________                                                                 
+        Monthly Payslip:             ${getFullName()}, ID: $employeeID                                                                                  
+    _______________________________________________________________________                                                                 
+        PAYMENT DETAILS (gross pay: ${getGrossMonthlyPay()})     
+             Salary: ${getMonthlySalary()}
+             Bonus:  ${getMonthlyBonus()}
+    ______________________________________________________________________
+        DEDUCTION DETAILS (total Deductions: ${getTotalMonthlyDeductions()})    
+             PAYE: ${getMonthlyPAYE()}
+             PRSI: ${getMonthlyPRSI()}
+             Cycle To Work: $cycleToWorkMonthlyDeduction
+    ______________________________________________________________________
+       NET PAY:${getNetMonthlyPay()}
+    _______________________________________________________________________""".trimMargin("|")
+
+fun getFullName() = when (gender){
+    'm', 'M' -> "Mr. $firstName $surname"
+    'f', 'F' -> "Ms. $firstName $surname"
+    else -> "$firstName $surname"
+}
+
+fun getMonthlySalary() = roundTwoDecimals(grossSalary / 12)
+fun getMonthlyPRSI() = roundTwoDecimals(getMonthlySalary() * (prsiPercentage / 100))
+fun getMonthlyPAYE() = roundTwoDecimals(getMonthlySalary() * (payePercentage / 100))
+fun getMonthlyBonus() = roundTwoDecimals((annualBonus / 12))
+fun getGrossMonthlyPay() = roundTwoDecimals(getMonthlySalary() + (annualBonus / 12))
+fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
+fun getNetMonthlyPay() = roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions())
+
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
